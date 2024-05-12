46b2a0e14caabc386fd0d29a41b042d784df0f65
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-05-12 23:23:11 +0900

    생성자

---
+++ java-basic/src/main/java/construct/MemberConstruct.java
@@ -0,0 +1,27 @@
+package construct;
+
+/**
+ * packageName : construct
+ * fileName : MemberConstruct
+ * author : hayj6
+ * date : 2024-05-07(007)
+ * description :
+ * 요약 :
+ * <p>
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-05-07(007)         hayj6          최초 생성
+ */
+public class MemberConstruct {
+    String name;
+    int age;
+    int grade;
+
+    MemberConstruct(String name, int age, int grade){
+        System.out.println("생성자 호출 name = " + name + ", age = " + age + ", grade = " + grade);
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+}
